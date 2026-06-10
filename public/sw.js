3a13ce23a2e3de5ddebf33f22a174ebb9129db30
ricardosouza07 <feniecosmeticos@gmail.com>
2026-06-10 06:03:16 -0300

    fix: estimador corrigido + cache PWA v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,14 @@
-const CACHE = 'fenipro-v1';
+const CACHE = 'fenipro-v2';
 const ASSETS = ['/'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
