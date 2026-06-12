db9653a9f1cd3875f42eb2ea225792f7a61832b1
ricardosouza07 <feniecosmeticos@gmail.com>
2026-06-12 19:06:29 -0300

    Bump PWA cache fenipro-v2 -> fenipro-v3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fenipro-v2';
+const CACHE = 'fenipro-v3';
 const ASSETS = ['/'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
